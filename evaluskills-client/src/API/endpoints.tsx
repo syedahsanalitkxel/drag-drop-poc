e64d70f34e8ea05ec801a136a7bf06b41f95e726
Usman Tahir <usman.tahir@tkxel.com>
2019-03-27 19:12:26 +0500

    changed api folder name

--- evaluskills-client/src/API/endpoints.tsx
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'http://5c9223ebe7b1a00014078cc7.mockapi.io/api/v1/';
-export const ASSESSMENTS = 'assessment';
-export const CLIENTS = 'client';
-export const INSTRUMENTS = 'instrument';
