224f0803f67f3058305cd534ba16af4ae5d57b2b
Usman Tahir <usman.tahir@tkxel.com>
2019-03-28 18:37:51 +0500

    eval-66 added whole page after refactor

--- evaluskills-client/src/components/pages/AddClient/index.tsx
+++ evaluskills-client/src/components/pages/AddClient/index.tsx
@@ -70,39 +70,141 @@ export const AddClient: React.FunctionComponent<Props> = ({ changeListener }) =>
     setFormState({ ...formState, contact });
   };
 
+  const renderContactList = (contact: any, index: number) => (
+    <Fragment key={index}>
+      <ClientContacts />
+    </Fragment>
+  );
+
   const renderForm = (formikprops: FormikBag) => {
-    return (
-      <Form onSubmit={formikprops.handleSubmit} className="form">
-        <PageBody card={true} className="m-t-15">
+    // TODO: Create render from group component and suppoert select, radio and checkbox
+    const renderFormGroup = (label: string, name: string, placeholder: string, last?: boolean) => {
+      return (
+        <React.Fragment>
           <FormGroup className="row">
-            <Label for="" className="col-sm-2 col-form-label font-bold">
-              Client Name
+            <Label for={name} className="col-sm-2 col-form-label font-bold">
+              {label}
             </Label>
             <div className="col-sm-3">
               <Input
-                name="clientName"
-                placeholder="Add Client Name"
+                name={name}
+                placeholder={placeholder}
                 tag={Field}
-                invalid={!!(formikprops.touched.clientName && formikprops.errors.clientName)}
+                id={name}
+                invalid={!!(formikprops.touched[name] && formikprops.errors[name])}
               />
+              <FormFeedback tooltip={true}>{formikprops.errors[name]}</FormFeedback>
+            </div>
+          </FormGroup>
+          {!last && <div className="hr-line-dashed" />}
+        </React.Fragment>
+      );
+    };
 
-              <FormFeedback tooltip={true}>{formikprops.errors.clientName}</FormFeedback>
+    return (
+      <Form onSubmit={formikprops.handleSubmit} className="form">
+        <PageBody card={true} className="m-t-15">
+          {renderFormGroup('Client Name', 'clientName', 'Add Client Name')}
+
+          <FormGroup className="row">
+            <Label className="col-sm-2 col-form-label font-bold">Logo</Label>
+            <div className="col-sm-3">
+              <img src="/img/Logo.svg" alt="logo" />
+              <span className="txt">Upload Photo</span>
             </div>
           </FormGroup>
           <div className="hr-line-dashed" />
+
+          {renderFormGroup('Address', 'address', 'Add Address')}
+          {renderFormGroup('City', 'city', 'Add City')}
+          {renderFormGroup('State', 'state', 'Add State')}
+          {renderFormGroup('Zip', 'zip', 'Add Zip')}
+          {renderFormGroup('School/Subsidiary', 'school', 'Add School')}
+          {renderFormGroup('Client Information', 'clientInformation', 'Add Client Information')}
+
+          <FormGroup className="row">
+            <Label for="billing" className="col-sm-2 col-form-label font-bold">
+              Billing
+            </Label>
+            <div className="col-sm-3">
+              <Input
+                type="select"
+                name="billing"
+                placeholder="Add Biling"
+                id="billing"
+                invalid={!!(formikprops.touched.billing && formikprops.errors.billing)}
+              >
+                <option value="billing-1">Biling 1</option>
+                <option value="billing-2">Biling 2</option>
+              </Input>
+
+              <FormFeedback tooltip={true}>{formikprops.errors.billing}</FormFeedback>
+            </div>
+          </FormGroup>
+          <div className="hr-line-dashed" />
+
+          <FormGroup className="row">
+            <label className="col-sm-2 col-form-label font-bold">Client Type</label>
+            <div className="col-sm-3">
+              <Input
+                type="select"
+                name="clientType"
+                placeholder="Client Type"
+                id="clientType"
+                invalid={!!(formikprops.touched.clientType && formikprops.errors.clientType)}
+              >
+                <option value="selected">Select Type</option>
+                <option value="co-oprate">Co-oprate</option>
+                <option value="Educational Institute">Educational Institute</option>
+              </Input>
+              <FormFeedback tooltip={true}>{formikprops.errors.clientType}</FormFeedback>
+            </div>
+          </FormGroup>
         </PageBody>
 
-        <div className="m-t-15 m-b-15">
-          <StyledButton type="button" size="lg">
-            Cancel
-          </StyledButton>
-          <StyledButton type="submit" color="primary" size="lg">
-            Save
-          </StyledButton>
-          <StyledButton type="button" color="primary" size="lg">
-            Save &amp; Add More
-          </StyledButton>
+        <div className="form-header row">
+          <div className="col-sm-6">
+            <h2>Contact</h2>
+          </div>
+          <div className="col-sm-6">
+            <Button
+              className="mt-3 float-right"
+              color="primary"
+              size="lg"
+              onClick={onClickAddContact}
+            >
+              Add Contact
+            </Button>
+          </div>
         </div>
+
+        <div>{formState.contact && formState.contact.map(renderContactList)}</div>
+
+        <div className="form-header row">
+          <div className="col-sm-6">
+            <h2>User Information</h2>
+          </div>
+        </div>
+
+        <PageBody card={true}>
+          {renderFormGroup('First Name', 'userFirstName', 'Add First Name')}
+          {renderFormGroup('Last Name', 'userLastName', 'Add Last Name')}
+          {renderFormGroup('Email', 'userEmail', 'Add Email', true)}
+        </PageBody>
+
+        <PageBody>
+          <div className="row m-b-25">
+            <StyledButton type="button" size="lg">
+              Cancel
+            </StyledButton>
+            <StyledButton type="submit" color="primary" size="lg">
+              Save
+            </StyledButton>
+            <StyledButton type="button" color="primary" size="lg">
+              Save &amp; Add More
+            </StyledButton>
+          </div>
+        </PageBody>
       </Form>
     );
   };
