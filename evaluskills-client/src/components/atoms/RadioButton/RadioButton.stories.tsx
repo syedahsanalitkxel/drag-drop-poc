a27bd891d82be54a24ce6ea8ad6cd016bde2e9d7
Usman Tahir <usman.tahir@tkxel.com>
2019-04-03 12:27:48 +0500

    eval-76 added add assessment button

--- evaluskills-client/src/components/atoms/RadioButton/RadioButton.stories.tsx
+++ evaluskills-client/src/components/atoms/RadioButton/RadioButton.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { FormGroup, Label } from 'reactstrap';
--- evaluskills-client/src/components/pages/AddEditInstrumentTemplate/index.tsx
+++ evaluskills-client/src/components/pages/AddEditInstrumentTemplate/index.tsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 
 import { Formik } from 'formik';
 
-import { Button, Form } from 'reactstrap';
+import { Button, Form, FormGroup, Label } from 'reactstrap';
 import AssessmentItemInterface from '../../../interfaces/AssessmentItem';
 import FormikBag from '../../../interfaces/FormikBag';
 import InstrumentTemplateInterface from '../../../interfaces/InstrumentTemplate';
 import PageBody from '../../atoms/PageBody';
 import PageHeader from '../../atoms/PageHeader';
+import RadioButton from '../../atoms/RadioButton';
 import FormElement, { FormElementTypes } from '../../molecules/FormElement';
+import Pager from '../../molecules/Pager';
 import ListCardItems from '../../organisms/ListCardItems';
 import DashboardTemplate from '../../templates/DashboardTemplate';
 
@@ -48,6 +50,15 @@ const AddEditInstrumentTemplate: React.FunctionComponent<Props> = ({ defaultValu
     setFormState({ ...formState, ...values });
   }
 
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFormState({ ...formState, status: JSON.parse(event.target.value) });
+  }
+
+  const showAssessmentSelector = (editable?: boolean) => (event: React.MouseEvent) => {
+    event.preventDefault();
+    console.log('clicked', editable);
+  };
+
   function renderForm(formikprops: FormikBag) {
     return (
       <Form onSubmit={formikprops.handleSubmit} className="form">
@@ -69,17 +80,50 @@ const AddEditInstrumentTemplate: React.FunctionComponent<Props> = ({ defaultValu
             type={FormElementTypes.TEXT_AREA}
           />
 
+          <FormGroup className="row">
+            <Label className="col-sm-2 col-form-label font-bold">Status</Label>
+            <div className="col-md-10">
+              <RadioButton
+                name="status"
+                value="true"
+                currentSelection={formState.status ? formState.status.toString() : ''}
+                onChange={handleChange}
+              >
+                Active
+              </RadioButton>
+              <RadioButton
+                name="status"
+                value="false"
+                currentSelection={formState.status !== undefined ? formState.status.toString() : ''}
+                onChange={handleChange}
+              >
+                Inactive
+              </RadioButton>
+            </div>
+          </FormGroup>
+          <div className="hr-line-dashed" />
+
           <FormElement
             label="Instrument Application"
             name="application"
             fullLength={true}
             formikprops={formikprops}
             type={FormElementTypes.SELECT}
+            last={!!defaultValues}
           >
             <option value="selected">Select Type</option>
             <option value="co-oprate">Co-oprate</option>
             <option value="Educational Institute">Educational Institute</option>
           </FormElement>
+
+          <div className="row">
+            <div className="col-sm-2 col-form-label font-bold">Assessment Items</div>
+            <div className="col-sm-10">
+              <Button onClick={showAssessmentSelector()} size="lg" color="primary">
+                Add Assessment Items
+              </Button>
+            </div>
+          </div>
         </PageBody>
       </Form>
     );
@@ -93,10 +137,15 @@ const AddEditInstrumentTemplate: React.FunctionComponent<Props> = ({ defaultValu
       </Formik>
       <div className="form-header row">
         <div className="col-sm-6">
-          <h2>Assessment Items {formState.assessmentItemsCount}</h2>
+          <h3 className="m-l-10 p-t-20">Assessment Items {formState.assessmentItemsCount}</h3>
         </div>
         <div className="col-sm-6">
-          <Button className="mt-3 float-right" color="primary" size="lg">
+          <Button
+            className="mt-3 float-right"
+            color="primary"
+            size="lg"
+            onClick={showAssessmentSelector(true)}
+          >
             Edit Assessment Items
           </Button>
         </div>
