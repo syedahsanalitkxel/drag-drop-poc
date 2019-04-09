64d54420ef8c0dc17e6b28c4a80f554b44ba3d22
Syed Ahsan Ali <ahsan.ali@tkxel.com>
2019-04-09 11:09:19 +0500

    eval-211 test issues fixed

--- evaluskills-client/src/components/pages/ParticipantEmailInvite/ParticipantEmailInvite.test.tsx
+++ evaluskills-client/src/components/pages/ParticipantEmailInvite/ParticipantEmailInvite.test.tsx
@@ -1,9 +1,10 @@
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
 import ParticipantEmailInvite from './index';
 
-describe('ParticipantEmailInvite', () => {
-  const PrtEmailInvite = shallow(<ParticipantEmailInvite />);
-  it('it renders correctly', () => {
-    expect(PrtEmailInvite).toMatchSnapshot();
-  });
+configure({ adapter: new Adapter() });
+const PrtEmailInvite = shallow(<ParticipantEmailInvite />);
+it('it renders correctly', () => {
+  expect(PrtEmailInvite).toMatchSnapshot();
 });
