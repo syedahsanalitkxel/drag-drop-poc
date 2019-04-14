cea98b6236ffcc1475bc18ceaa7d952cbee5565c
Usman Tahir <usman.tahir@tkxel.com>
2019-04-15 03:46:06 +0500

    eval-221 added route guards

--- evaluskills-client/src/App.tsx
+++ evaluskills-client/src/App.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 import LookupContainer from './containers/LookupContainer';
+import AuthContextContainer from './modules/Auth/container';
 import Routes from './Routes';
 
 const App = () => (
   <div id="wrapper">
     <ErrorBoundary>
-      <LookupContainer>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </LookupContainer>
+      <BrowserRouter>
+        <AuthContextContainer>
+          <LookupContainer>
+            <Routes />
+          </LookupContainer>
+        </AuthContextContainer>
+      </BrowserRouter>
     </ErrorBoundary>
   </div>
 );
