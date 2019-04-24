6eb40fd592ae226abd883d3868ce4691404cd238
qasim <muhammad.qasim@tkxel.com>
2019-04-24 13:04:33 +0500

    EVAL-284: Add and Edit Email Templates for Client Admin User

--- evaluskills-client/src/utils/index.tsx
+++ evaluskills-client/src/utils/index.tsx
@@ -10,6 +10,15 @@ function checkRole(role: string) {
   return false;
 }
 
+export const getActiveClient = () => {
+  const rawUser = localStorage.getItem('user');
+
+  if (rawUser) {
+    const parsedUser = JSON.parse(rawUser);
+    return parsedUser.activeClientId;
+  }
+};
+
 export const USER_ROLE = {
   isClientAdmin: () => checkRole(userType.CLIENT_ADMIN),
   isSuperAdmin: () => checkRole(userType.SUPER_ADMIN),
