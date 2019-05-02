1497bea12d50ce11c12f2843054e1c8c2e4f25a1
Syed Ahsan Ali <ahsan.ali@tkxel.com>
2019-05-02 19:53:43 +0500

    eval-254  validations added

--- evaluskills-client/src/components/pages/CreateInstruments/clientFormSchema.tsx
+++ evaluskills-client/src/components/pages/CreateInstruments/clientFormSchema.tsx
@@ -1,82 +1,72 @@
 import * as Yup from 'yup';
 
 const evaluationFormSchema = Yup.object().shape({
-  // title: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // dueDate: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // sendInstrument:Yup.string()
-  //   .required('Required'),
-  // allowParticipantsToAddEvaluators:Yup.string()
-  //   .required('Required'),
-  // date1: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // date2: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // date3: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // emailTemplate1: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // emailTemplate2: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
-  // emailTemplate3: Yup.string()
-  //   .min(2, 'Too Short!')
-  //   .max(50, 'Too Long!')
-  //   .required('Required'),
+  title: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  dueDate: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  sendInstrument: Yup.string().required('Required'),
+  allowParticipantsToAddEvaluators: Yup.string().required('Required'),
+  date1: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  date2: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  date3: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  emailTemplate1: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  emailTemplate2: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  emailTemplate3: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
   participants: Yup.array().of(
     Yup.object().shape({
-      participant: Yup.object().shape({
-        // firstName: Yup.string()
-        //   .min(2, 'Too Short!')
-        //   .max(50, 'Too Long!'),
-        //   // .required('Required'),
-        // lastName: Yup.string()
-        //   .min(2, 'Too Short!')
-        //   .max(50, 'Too Long!'),
-        //   // .required('Required'),
-        // email: Yup.string()
-        //   .email('Invalid email')
-        //   .min(2, 'Too Short!')
-        //   .max(50, 'Too Long!'),
-        //   // .required('Required'),
-        // roleId: Yup.string()
-        //   .min(2, 'Too Short!')
-        //   .max(50, 'Too Long!'),
-        // .required('Required'),
-      }),
-      evaluator: Yup.array().of(
+      firstName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      lastName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email')
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      roleId: Yup.string().required('Required'),
+      evaluators: Yup.array().of(
         Yup.object().shape({
-          // firstName: Yup.string()
-          //   .min(2, 'Too Short!')
-          //   .max(50, 'Too Long!'),
-          //   // .required('Required'),
-          // lastName: Yup.string()
-          //   .min(2, 'Too Short!')
-          //   .max(50, 'Too Long!'),
-          //   // .required('Required'),
-          // email: Yup.string()
-          //   .email('Invalid email')
-          //   .min(2, 'Too Short!')
-          //   .max(50, 'Too Long!'),
-          // .required('Required'),
-          // roleId: Yup.string()
-          //   .min(2, 'Too Short!')
-          //   .max(50, 'Too Long!'),
-          // .required('Required'),
+          firstName: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+          lastName: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email')
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+          roleId: Yup.string().required('Required'),
         })
       ),
     })
